525ba081c25a85296eafc3533d9daa02cd51f07d
Lucas <lucas.miquet.westphal@gmail.com>
2025-04-02 16:44:25 -0400

    Removed the model file, use the td3 model now, added model saving

--- Start 1.py
+++ Start 1.py
@@ -4,7 +4,15 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 from Functions import *
 from RLEnvironment import TradingEnv
+import torch
+import gymnasium as gym
+from stable_baselines3 import TD3
+from stable_baselines3.common.noise import NormalActionNoise
 
+print(torch.cuda.is_available())  # should be True
+print(torch.cuda.get_device_name(0))  # should show your GPU
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 raw_data = yf.download("AAPL", start="2020-01-01", end="2021-01-01")
 raw_data.columns = raw_data.columns.droplevel('Ticker')
 df = modelInput(raw_data)
@@ -16,20 +24,22 @@ features = [
 ]
 
 # Create the environment with debug mode on
-env = TradingEnv(df, feature_cols=features, verbose=True)
+env = TradingEnv(df, feature_cols=features, verbose=False)
 
-# Reset the environment
-obs, _ = env.reset()
-print("Initial Observation:", obs)
+n_actions = env.action_space.shape[0]
+action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
-for episode in range(100):
-    obs, _ = env.reset()
-    done = False
-    total_reward = 0
+# Create the model
+model = TD3(
+    "MlpPolicy",
+    env,
+    action_noise=action_noise,
+    verbose=1,
+    tensorboard_log="./td3_logs/",
+)
 
-    while not done:
-        action = env.action_space.sample()
-        obs, reward, done, _, _ = env.step(action)
-        total_reward += reward
+# Train the model
+model.learn(total_timesteps=100_000)
 
-    print(f"Episode {episode + 1} completed — Total reward: {total_reward:.4f}")
+# Save the model
+model.save("td3_trading_model")
